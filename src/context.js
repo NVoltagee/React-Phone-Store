b481d6272bea7ef6001eaa89608fe7fa316ad5c6
Norman Trujillo <natrujillo9@gmail.com>
2019-02-11 23:45:36 -0500

    Ran Prettier to clean code.

--- src/context.js
+++ src/context.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react'
-import {storeProducts, detailProduct} from './data'
+import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
 const ProductContext = React.createContext();
 
-
 class ProductProvider extends Component {
-    state={
-        products:storeProducts, detailProduct:detailProduct
-    }
-    handleDetail = () => {
-        console.log('Hello from detail');
-    }
-    addToCart = () => {
-        console.log('Hello from add to cart');
-    }
-    render() {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
+  handleDetail = () => {
+    console.log("Hello from detail");
+  };
+  addToCart = () => {
+    console.log("Hello from add to cart");
+  };
+  render() {
     return (
-      <ProductContext.Provider value={{
-       ...this.state,
-       handleDetail: this.handleDetail,
-       addToCart: this.addToCart
-      }}>
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
-    )
+    );
   }
 }
 
 const ProductConsumer = ProductContext.Consumer;
 
-export {ProductConsumer, ProductProvider};
+export { ProductConsumer, ProductProvider };
